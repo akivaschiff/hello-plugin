2ad3eea86cac9ffffce3b18a1d99b97129702711
GitHub Action <action@github.com>
2023-01-02 18:52:40 +0000

    Template cleanup

--- src/main/kotlin/com/github/akivaschiff/helloplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/akivaschiff/helloplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.akivaschiff.helloplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.akivaschiff.helloplugin.MyBundle
 
 class MyApplicationService {
 
